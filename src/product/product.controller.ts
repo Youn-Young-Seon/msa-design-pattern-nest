17e16555e2794cbfcd64f359e8811060eceba9fe
szdbsyyszs <szdbsyyszs32@gmail.com>
2025-10-28 00:46:14 +0900

    나머지 부분 완료 전체 페이지 확인 필요 파라미터에서 에러나는 경우 발생

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Redirect, Render } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { Product } from './domain/product';
 
-@Controller('product')
+@Controller()
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  @Redirect("/products")
+  home() {
+    return;
+  }
+
+  @Get('products')
+  @Render('product/list')
+  async listProducts(
+    @Query('category') category: string, 
+    @Query('manufacturer') manufacturer: string
+  ) {
+    let products: Product[];
+
+    if (category && manufacturer) {
+      products = await this.productService.getProductsByManufacturerAndCategory(manufacturer, category);
+    } else if (category) {
+      products = await this.productService.getProductsByCategory(category);
+    } else if (manufacturer) {
+      products = await this.productService.getProductsByManufacturer(manufacturer);
+    } else {
+      products = await this.productService.getAllProducts();
+    }
+
+    return {
+      products,
+      category,
+      manufacturer,
+    }
+  }
 }
